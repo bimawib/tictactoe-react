20057f328d578a642d56d2ac0eeb379b2b19434e
Bima Sahidsyah Putra Wibowo <bimaencun@gmail.com>
2023-06-03 20:35:39 +0700

    configuring tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
 
-function Board(){
+function TextHeader(){
   return (
-    <div className='justify-items-center' >
-      <h1 className='text-sky-500 bg-lime-700'>TicTacToe</h1>
+    <div>
+      <h1 className='text-indigo-800 bg-teal-300 w-48 h-24 mx-auto text-center'>TicTacToe</h1>
+    </div>
+  );
+}
+
+function Board(){
+  function Area(i){
+    return (
+      <button className='w-24 h-24 border-4 border-indigo-200'></button>
+    );
+  }
+
+  return (
+    <div className='bg-teal-500 w-3/4 h- shadow-lg mx-auto'>
+        <div className='flex justify-center'>
+          <Area/>
+          <Area/>
+          <Area/>
+        </div>
+        <div className='flex justify-center'>
+          <Area/>
+          <Area/>
+          <Area/>
+        </div>
+        <div className='flex justify-center'>
+          <Area/>
+          <Area/>
+          <Area/>
+        </div>
     </div>
   );
 }
@@ -11,6 +39,7 @@ function Board(){
 function App() {
   return (
     <div >
+      <TextHeader/>
       <Board/>
     </div>
   );
