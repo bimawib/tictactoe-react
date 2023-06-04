13035eb4fa4eaadec893cb99b482c5cc89283469
Bima Sahidsyah Putra Wibowo <bimaencun@gmail.com>
2023-06-04 16:00:09 +0700

    (feat): fix tictactoe redux toolkit, still has bug: board is checked 1 step behind

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+// import { useState, useEffect, createContext, useContext } from 'react';
+import store from './store';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+import { addInput, resetState, setWinner } from './reduxToolkit';
 
 function TextHeader(){
   return (
@@ -9,39 +13,97 @@ function TextHeader(){
 }
 
 function InfoPanel(){
+  const winner = useSelector((state) => state.game.winner);
+  const turn = useSelector((state) => state.game.turn);
+
   return (
     <div className='text-slate-100 bg-teal-900 w-1/4 mx-auto text-center text-5xl font-serif my-4 h-full rounded-lg py-6'>
-      <h1>Next Turn : O</h1>
+      <h1>Next Turn : {turn%2 === 0 ? 'O' : 'X'}</h1>
       <span className='rounded-sm bg-yellow-400'>
-      <button>Reset</button>
+      {winner !== '' ? <WinnerModal/> :''}
+      </span>
+    </div>
+  );
+}
+
+function WinnerModal(){
+  const winner = useSelector((state) => state.game.winner);
+  const dispatch = useDispatch();
+
+  return (
+    <div className='text-slate-100 bg-teal-900 w-1/4 mx-auto text-center text-5xl font-serif my-4 h-full rounded-lg py-6'>
+      <h1>Winner : {winner}</h1>
+      <span className='rounded-sm bg-yellow-400'>
+      {winner !== '' ? <button className='bg-teal-900 text-slate-100 rounded-sm px-2 py-1' onClick={() => dispatch(resetState())}>Play Again</button> : ''}
       </span>
     </div>
   );
 }
 
 function Board(){
+  const dispatch = useDispatch();
+  const turn = useSelector((state) => state.game.turn);
+  const playerInput = useSelector((state) => state.game.playerInput);
+  const playerO = playerInput[0];
+  const playerX = playerInput[1];
+  const winner = useSelector((state) => state.game.winner);
+
+  function CheckWinner(boxNumber){
+    const winningCombos = [
+      [1, 2, 3], [4, 5, 6], [7, 8, 9], // horizontal
+      [1, 4, 7], [2, 5, 8], [3, 6, 9], // vertical
+      [1, 5, 9], [3, 5, 7] // diagonal
+    ];
+    for(let i = 0; i < winningCombos.length; i++){
+      if(playerO.includes(winningCombos[i][0]) && playerO.includes(winningCombos[i][1]) && playerO.includes(winningCombos[i][2])){
+        dispatch(setWinner({winner: 'O'}));
+      } else if(playerX.includes(winningCombos[i][0]) && playerX.includes(winningCombos[i][1]) && playerX.includes(winningCombos[i][2])){
+        dispatch(setWinner({winner: 'X'}));
+      } else if(turn === 9){
+        dispatch(setWinner({winner: 'Draw'}));
+      }
+    }
+  }
+
+  function HandleClick(index){
+    if(playerO.includes(index) || playerX.includes(index)){
+      alert('This area is already taken');
+    } else {
+      CheckWinner(index);
+      console.log(index);
+      console.log(winner);
+      console.log(playerO);
+      console.log(playerX);
+      dispatch(addInput({inputIndex: index, turn: turn}));
+    }
+  }
+
   function Area(i){
+    const boxNumber = i.boxNumber;
     return (
-      <button className='w-32 h-32 border-4 text-white border-stone-400 border-x-stone-500 rounded-sm'>X</button>
+      <button className='w-32 h-32 border-4 text-4xl text-white border-stone-400 border-x-stone-500 rounded-sm' onClick={() => HandleClick(boxNumber)}>
+        {playerO.includes(boxNumber) ? 'O' : playerX.includes(boxNumber) ? 'X' : ''}
+        {/* {i.boxNumber} */}
+      </button>
     );
   }
 
   return (
     <div className='bg-slate-800 w-96 shadow-xl mx-auto rounded-lg h-full'>
         <div className='flex justify-center w-full'>
-          <Area/>
-          <Area/>
-          <Area/>
+          <Area key={1} boxNumber={1}/>
+          <Area key={2} boxNumber={2}/>
+          <Area key={3} boxNumber={3}/>
         </div>
         <div className='flex justify-center w-full'>
-          <Area/>
-          <Area/>
-          <Area/>
+          <Area key={4} boxNumber={4}/>
+          <Area key={5} boxNumber={5}/>
+          <Area key={6} boxNumber={6}/>
         </div>
         <div className='flex justify-center w-full'>
-          <Area/>
-          <Area/>
-          <Area/>
+          <Area key={7} boxNumber={7}/>
+          <Area key={8} boxNumber={8}/>
+          <Area key={9} boxNumber={9}/>
         </div>
     </div>
   );
@@ -49,10 +111,12 @@ function Board(){
 
 function App() {
   return (
-    <div >
-      <TextHeader/>
-      <Board/>
-      <InfoPanel/>
+    <div>
+      <Provider store={store}>
+        <TextHeader/>
+          <Board/>
+        <InfoPanel/>
+      </Provider>
     </div>
   );
 }
--- src/reduxToolkit.js
+++ src/reduxToolkit.js
@@ -15,49 +15,19 @@ const gameSlicer = createSlice({
     addInput(state, action){
       if(action.payload.turn % 2 === 0){
         state.playerInput[0].push(action.payload.inputIndex);
-        if(state.turn >= 4){
-          state.compareInput();
-        }
         state.turn++;
       } else {
         state.playerInput[1].push(action.payload.inputIndex);
         state.turn++;
       }
     },
-    compareInput(state){
-      const winningCombos = [
-        [0, 1, 2], 
-        [3, 4, 5], 
-        [6, 7, 8], 
-        [0, 3, 6], 
-        [1, 4, 7], 
-        [2, 5, 8], 
-        [0, 4, 8], 
-        [2, 4, 6]
-      ];
-      const playerInput = state.playerInput;
-      const playerO = playerInput[0];
-      const playerX = playerInput[1];
-      let winner = '';
-      for(let i = 0; i < winningCombos.length; i++){
-        if(playerO.includes(winningCombos[i][0]) && playerO.includes(winningCombos[i][1]) && playerO.includes(winningCombos[i][2])){
-          winner = 'O';
-        } else if(playerX.includes(winningCombos[i][0]) && playerX.includes(winningCombos[i][1]) && playerX.includes(winningCombos[i][2])){
-          winner = 'X';
-        }
-      }
-      if(winner === 'O'){
-        state.winner = 'O';
-      } else if(winner === 'X'){
-        state.winner = 'X';
-      } else if(state.turn === 9){
-        state.winner = 'draw';
-      }
-    },
     resetState(state){
       state.winner = '';
       state.playerInput = [[], []];
       state.turn = 0;
+    },
+    setWinner(state, action){
+      state.winner = action.payload.winner;
     }
   }
 });
@@ -67,6 +37,6 @@ const gameSlicer = createSlice({
 // if the length of playerInput is 9 and no winning combinations are found, then it's a draw
 // if the length of playerInput is less than 9 and no winning combinations are found, then the game continues
 
-export const { addInput, compareInput, resetState } = gameSlicer.actions;
+export const { addInput, resetState, setWinner } = gameSlicer.actions;
 
 export default gameSlicer.reducer;
