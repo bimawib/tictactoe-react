e9d622226bb91dc6c209788b542eba79e3c4fbfa
Bima Sahidsyah Putra Wibowo <bimaencun@gmail.com>
2023-06-04 12:58:17 +0700

    finishing redux toolkit for reduccers

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function TextHeader(){
   return (
     <div>
-      <h1 className='text-indigo-800 bg-teal-300 w-48 h-24 mx-auto text-center'>TicTacToe</h1>
+      <h1 className='text-slate-100 bg-teal-900 w-1/4 mx-auto text-center text-5xl font-serif my-4 h-full rounded-lg'>Tic~Tac~Toe</h1>
+    </div>
+  );
+}
+
+function InfoPanel(){
+  return (
+    <div className='text-slate-100 bg-teal-900 w-1/4 mx-auto text-center text-5xl font-serif my-4 h-full rounded-lg py-6'>
+      <h1>Next Turn : O</h1>
+      <span className='rounded-sm bg-yellow-400'>
+      <button>Reset</button>
+      </span>
     </div>
   );
 }
@@ -11,23 +22,23 @@ function TextHeader(){
 function Board(){
   function Area(i){
     return (
-      <button className='w-24 h-24 border-4 border-indigo-200'></button>
+      <button className='w-32 h-32 border-4 text-white border-stone-400 border-x-stone-500 rounded-sm'>X</button>
     );
   }
 
   return (
-    <div className='bg-teal-500 w-3/4 h- shadow-lg mx-auto'>
-        <div className='flex justify-center'>
+    <div className='bg-slate-800 w-96 shadow-xl mx-auto rounded-lg h-full'>
+        <div className='flex justify-center w-full'>
           <Area/>
           <Area/>
           <Area/>
         </div>
-        <div className='flex justify-center'>
+        <div className='flex justify-center w-full'>
           <Area/>
           <Area/>
           <Area/>
         </div>
-        <div className='flex justify-center'>
+        <div className='flex justify-center w-full'>
           <Area/>
           <Area/>
           <Area/>
@@ -41,6 +52,7 @@ function App() {
     <div >
       <TextHeader/>
       <Board/>
+      <InfoPanel/>
     </div>
   );
 }
--- src/reduxToolkit.js
+++ src/reduxToolkit.js
@@ -3,5 +3,70 @@ import {
 } from '@reduxjs/toolkit';
 
 const initialState = {
-  
+  winner: '',
+  playerInput: [[], []],
+  turn: 0
 }
+
+const gameSlicer = createSlice({
+  name: 'tictactoe',
+  initialState,
+  reducers: {
+    addInput(state, action){
+      if(action.payload.turn % 2 === 0){
+        state.playerInput[0].push(action.payload.inputIndex);
+        if(state.turn >= 4){
+          state.compareInput();
+        }
+        state.turn++;
+      } else {
+        state.playerInput[1].push(action.payload.inputIndex);
+        state.turn++;
+      }
+    },
+    compareInput(state){
+      const winningCombos = [
+        [0, 1, 2], 
+        [3, 4, 5], 
+        [6, 7, 8], 
+        [0, 3, 6], 
+        [1, 4, 7], 
+        [2, 5, 8], 
+        [0, 4, 8], 
+        [2, 4, 6]
+      ];
+      const playerInput = state.playerInput;
+      const playerO = playerInput[0];
+      const playerX = playerInput[1];
+      let winner = '';
+      for(let i = 0; i < winningCombos.length; i++){
+        if(playerO.includes(winningCombos[i][0]) && playerO.includes(winningCombos[i][1]) && playerO.includes(winningCombos[i][2])){
+          winner = 'O';
+        } else if(playerX.includes(winningCombos[i][0]) && playerX.includes(winningCombos[i][1]) && playerX.includes(winningCombos[i][2])){
+          winner = 'X';
+        }
+      }
+      if(winner === 'O'){
+        state.winner = 'O';
+      } else if(winner === 'X'){
+        state.winner = 'X';
+      } else if(state.turn === 9){
+        state.winner = 'draw';
+      }
+    },
+    resetState(state){
+      state.winner = '';
+      state.playerInput = [[], []];
+      state.turn = 0;
+    }
+  }
+});
+
+// create a function to compare both arrays in playerInput to an array of winning combinations
+// if any of the winning combinations are found in playerInput, then the player wins
+// if the length of playerInput is 9 and no winning combinations are found, then it's a draw
+// if the length of playerInput is less than 9 and no winning combinations are found, then the game continues
+
+export const { addInput, compareInput, resetState } = gameSlicer.actions;
+
+export default gameSlicer.reducer;
